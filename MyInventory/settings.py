0eead1bc40270aab718d91714a24504655cab09c
Osama Khan <usamak552@gmail.com>
2022-10-22 21:35:30 +0530

    cc

--- MyInventory/settings.py
+++ MyInventory/settings.py
@@ -85,12 +85,24 @@ WSGI_APPLICATION = 'MyInventory.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+     "default": {
+        "ENGINE": "django.db.backends.postgresql",
+         "NAME": "postgres",
+         "USER": "osama",
+         "PASSWORD": "root123@",
+         "HOST": "cbinventory.postgres.database.azure.com",
+         "PORT": "5432",
+         "OPTIONS":{"sslmode":"require"}
+     },
+ }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
